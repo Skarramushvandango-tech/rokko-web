914c22a0b74d794731073fa59560309d70ec3ae4
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2026-05-26 21:49:52 +0200

    Add files via upload

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -1,3 +1,5 @@
+import { asset } from "../utils/asset";
+
 export interface ArtistLinks {
   appleMusic?: string;
   spotify?: string;
@@ -22,8 +24,8 @@ export const ARTISTS: Artist[] = [
     name: "Sukram",
     nameH3: "Sukram",
     nameH1: "",
-    image: "/assets/artist-images/sukram-pop.png",
-    cover: "/assets/coverartwork/sukram-cover.png",
+    image: asset("/assets/artist-images/sukram-pop.png"),
+    cover: asset("/assets/coverartwork/sukram-cover.png"),
     albumTitle: "D. mein Fiebertraum",
     bio: "Der böse Bube liefert uns mit – Deutschland mein Fiebertraum – eine psychedelische Indie-Rock Experience der Extraklasse. Seine Lyrics sind treffsicher ohne zu beleidigen. Vielmehr ist es ein Klagelied, welches Sukram hier zum Besten gibt. Ein Klagelied über ein Heimatland, das seine Bewohner wie Nutzvieh über den Hof treibt, um auch das letzte Bisschen Glück und Wohlstand aus der Gesellschaft zu saugen.",
     links: {
@@ -37,8 +39,8 @@ export const ARTISTS: Artist[] = [
     name: "SkaRamush Vandango",
     nameH3: "SkaRamush",
     nameH1: "Vandango",
-    image: "/assets/artist-images/skaramush-vandango-pop.png",
-    cover: "/assets/coverartwork/vandango-cover.png",
+    image: asset("/assets/artist-images/skaramush-vandango-pop.png"),
+    cover: asset("/assets/coverartwork/vandango-cover.png"),
     albumTitle: "Neurocentric",
     bio: "Der Gründer und Labelchef von ROKKO! Records ist selbst als Künstler aktiv. Mit seinem Projekt SkaRamush Vandango verbindet er elektronische Sounds mit roher Energie. Neurocentric steht für seine Fähigkeit, komplexe Themen in mitreißende Tracks zu verwandeln.",
     links: {
@@ -52,8 +54,8 @@ export const ARTISTS: Artist[] = [
     name: "Skank Schablonski",
     nameH3: "Skank",
     nameH1: "Schablonski",
-    image: "/assets/artist-images/skank-schablonski-pop.png",
-    cover: "/assets/coverartwork/schablonski-cover.png",
+    image: asset("/assets/artist-images/skank-schablonski-pop.png"),
+    cover: asset("/assets/coverartwork/schablonski-cover.png"),
     albumTitle: "Kohle raus",
     bio: "Skank Schablonski steht für kompromisslosen Sound und ehrliche Texte. Mit Kohle raus liefert er einen Track, der keine Gefangenen macht. Schablonski's Stil ist einzigartig – roh, direkt und unverkennbar.",
     links: {
@@ -67,8 +69,8 @@ export const ARTISTS: Artist[] = [
     name: "Fléur et Beunié",
     nameH3: "Fléur et",
     nameH1: "Beunié",
-    image: "/assets/artist-images/fleur-beunie-pop.png",
-    cover: "/assets/coverartwork/beunie-cover.png",
+    image: asset("/assets/artist-images/fleur-beunie-pop.png"),
+    cover: asset("/assets/coverartwork/beunie-cover.png"),
     albumTitle: "Feu Léger",
     bio: "Das französische House-Duo lieferte mit Feu Léger 2025 eine French-House-Hymne der Extraklasse. Selbst die Remixe überzeugen leichtfüßig. Es ist aber nicht nur die Liebe zur Musik, welche Fléur und Beunié des Nachts zueinander treibt – die beiden sind auch privat liiert.",
     links: {
@@ -82,8 +84,8 @@ export const ARTISTS: Artist[] = [
     name: "Anthony Sinclair",
     nameH3: "Anthony",
     nameH1: "Sinclair",
-    image: "/assets/artist-images/anthony-sinclair-pop.png",
-    cover: "/assets/coverartwork/sinclair-cover.png",
+    image: asset("/assets/artist-images/anthony-sinclair-pop.png"),
+    cover: asset("/assets/coverartwork/sinclair-cover.png"),
     albumTitle: "waste",
     bio: "Anthony Sinclair ist einer der charakterstärksten Stimmen im ROKKO! Roster. Mit waste liefert er einen Track, der unter die Haut geht – eine Auseinandersetzung mit Verschwendung auf allen Ebenen. Sinclair's Musik ist so präzise wie sein Name.",
     links: {
@@ -97,8 +99,8 @@ export const ARTISTS: Artist[] = [
     name: "Silberstreif",
     nameH3: "Silberstreif",
     nameH1: "",
-    image: "/assets/artist-images/silberstreif-pop.png",
-    cover: "/assets/coverartwork/silberstreif-cover.png",
+    image: asset("/assets/artist-images/silberstreif-pop.png"),
+    cover: asset("/assets/coverartwork/silberstreif-cover.png"),
     albumTitle: "Flammend Herz",
     bio: "Silberstreif steht für Hoffnung in düsteren Zeiten. Das Debütalbum Flammend Herz verbrennt alle Brücken zur Mittelmäßigkeit. Mit einer Mischung aus Emotion und handwerklicher Präzision schafft Silberstreif Musik, die nachwirkt.",
     links: {
@@ -112,8 +114,8 @@ export const ARTISTS: Artist[] = [
     name: "Anger Uschis",
     nameH3: "Anger",
     nameH1: "Uschis",
-    image: "/assets/artist-images/anger-uschis-pop.png",
-    cover: "/assets/coverartwork/uschis-cover.png",
+    image: asset("/assets/artist-images/anger-uschis-pop.png"),
+    cover: asset("/assets/coverartwork/uschis-cover.png"),
     albumTitle: "politisch inkontinent",
     bio: `Mit dem Debütalbum \u201epolitisch inkontinent\u201c liefert Anger Uschis ein politisch orientiertes Mahnmal gegen Hass, Hetze und Faschismus. Einige Songs wurden bereits zu kleinen Hymnen, wie \u201elachen f*cken schreien\u201c, der sich mit den niederen Gelüsten der Gesellschaft auseinandersetzt. Multifunktionslos bildet auf dem Album das Stück, das der Dummheit textlich auf sehr hoher Ebene gegenübertritt. \u2013 The Uschis have the high ground`,
     links: {
@@ -127,8 +129,8 @@ export const ARTISTS: Artist[] = [
     name: "Henri Bellieu",
     nameH3: "Henri",
     nameH1: "Bellieu",
-    image: "/assets/artist-images/henri-bellieu-pop.png",
-    cover: "/assets/coverartwork/bellieu-cover.png",
+    image: asset("/assets/artist-images/henri-bellieu-pop.png"),
+    cover: asset("/assets/coverartwork/bellieu-cover.png"),
     albumTitle: "La Femme",
     bio: `Der charmant-markante Franzose liefert mit seiner Single La Femme bereits seine zweite Ode an die Weiblichkeit. Henri, der ab und an in Pariser Bars und Kneipen stand, lernte ROKKO!'s Labelchef SkaRamush bereits 2015 in Paris kennen. Die bis heute andauernde Freundschaft sorgte dann 2025 mit der Debütsingle \u201epetite Colibri\u201c für seinen Einstieg in die Welt der Popmusik.`,
     links: {
