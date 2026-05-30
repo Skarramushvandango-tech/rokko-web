6fcb9016182585ea5f6fadbe96d69e50500e008e
Skarramushvandango-tech <Skarramush.vandango@gmx.de>
2026-05-30 20:20:23 +0200

    main: Impressum mit echten Daten befüllt (Markus Ilgner)

--- src/components/ImpressumModal.tsx
+++ src/components/ImpressumModal.tsx
@@ -15,21 +15,23 @@ export default function ImpressumModal({ onClose }: ImpressumModalProps) {
         <div className="legal-body">
           <h3>Angaben gemäß § 5 TMG</h3>
           <p>
+            Markus Ilgner<br />
             ROKKO! Records<br />
-            [Straße und Hausnummer]<br />
-            [PLZ] [Stadt]<br />
+            Am Brunnen 3<br />
+            Duisburg<br />
             Deutschland
           </p>
 
           <h3>Kontakt</h3>
           <p>
-            E-Mail: [E-Mail-Adresse]
+            E-Mail: info@rokko-records.de
           </p>
 
           <h3>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h3>
           <p>
-            [Name des Verantwortlichen]<br />
-            [Adresse wie oben]
+            Markus Ilgner<br />
+            Am Brunnen 3<br />
+            Duisburg
           </p>
 
           <h3>Haftungsausschluss</h3>
@@ -42,7 +44,7 @@ export default function ImpressumModal({ onClose }: ImpressumModalProps) {
           <h4>Urheberrecht</h4>
           <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.</p>
 
-          <p className="legal-date">Stand: Mai 2025</p>
+          <p className="legal-date">Stand: Mai 2026</p>
         </div>
       </div>
     </div>
